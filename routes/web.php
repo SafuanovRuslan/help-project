24fa6ab48a90ef078c44c2e5288d179144bf71f8
Safuanov Ruslan <dwarf3.banki.shop@mail.ru>
2024-11-18 20:39:15 +0300

    commit 6

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return redirect('/');
+});
+
 Route::get('/', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('/');
